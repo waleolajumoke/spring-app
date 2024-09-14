8e47adfa8e1da20984c50900c8e1426b22ee25fb
waleolajumoke <waleolajumoke@gmail.com>
2024-09-14 10:37:59 +0100

    uploading the java app

---
+++ src/main/java/com/example/tech365app/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.tech365app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to Java!";
+    }
+}
---
+++ src/main/java/com/example/tech365app/Tech365appApplication.java
@@ -0,0 +1,13 @@
+package com.example.tech365app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech365appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech365appApplication.class, args);
+	}
+
+}
